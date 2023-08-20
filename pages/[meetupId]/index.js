c9116ede7b840348736e0543e5523420ae70dde4
Cgs1208 <charangs157@gmail.com>
2023-08-20 16:12:07 +0530

    some comments added

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -32,7 +32,8 @@ export async function getStaticPaths() {
   client.close();
 
   return {
-    fallback: "blocking", //true-first emty page and then get data , blocking-user will not see anything until page is generated with data
+    fallback: "blocking", //true-first emty page and then get data ,
+    // blocking-user will not see anything until page is generated with data
     paths: meetups.map((meetup) => ({
       params: { meetupId: meetup._id.toString() },
     })),
